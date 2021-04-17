155030374dec25b4b61bbb2bc0376a7dd79c7265
muwoo <2424880409@qq.com>
2021-04-17 23:49:57 +0800

    ref: 部署

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  configureWebpack: config => {
+    config.optimization = {
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 300000, // 依赖包超过300000bit将被单独打包
+        automaticNameDelimiter:'-',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `chunk.${packageName.replace('@', '')}`;
+            },
+            priority:10
+          }
+        }
+      }
+    }
+  }
+
+}
